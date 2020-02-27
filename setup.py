c5112d004dc2123aae68d5cfa7bba246ae6c8c8d
Yeremia Andi Irawan <yeremia@accelbyte.net>
2020-02-27 11:31:07 +0700

    Initial commit

---
+++ setup.py
@@ -0,0 +1,16 @@
+import setuptools
+
+setuptools.setup(
+    name='justice_iam_python_sdk',
+    packages=['justice_iam_python_sdk'],
+    version='0.1',
+    author="Yeremia Andi Irawan",
+    author_email="",
+    description="Justice IAM Python SDK",
+    url="https://github.com/lunarray/justice-iam-python-sdk",
+    classifiers=[
+     "Programming Language :: Python :: 3",
+     "License :: OSI Approved :: Apache 2.0 License",
+     "Operating System :: OS Independent",
+    ],
+)
